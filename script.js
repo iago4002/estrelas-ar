4cea3546939f898a3bc16bed81bc7d9727b02036
Matheus Alves <math.alvesm@gmail.com>
2020-05-05 09:10:25 -0300

    postion

--- script.js
+++ script.js
@@ -45,9 +45,10 @@ window.onload = () => {
                     const longitude = place.location.lng;                    
                     // add place name
                     const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', `altitude: 500; latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
+                    placeText.setAttribute('position', '0 200 0');
                     
                     placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
